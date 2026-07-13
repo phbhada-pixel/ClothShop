2964a2d98695fe32968d976f2f182d0b3f4a0859
phbhada-pixel <phbhada@gmail.com>
2026-07-13 23:46:37 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Supabase ची माहिती (Settings > API मधून कॉपी केलेली)
-const SUPABASE_URL = "https://oubykgikizuoaogioujf.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_Vu42iQC-e2tJRKyxjhQzEg_bQBxPGF6";
-
-// Supabase क्लायंट चालू करणे
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
